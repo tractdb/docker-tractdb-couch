63f835e2b5fc04a467aeac4f45c21c6b31ed78ed
James Fogarty <jayfo.github@gmail.com>
2015-03-27 20:16:31 -0700

    Convert to Use Wrapper / Secrets
    Convert to use a wrapper script and the secrets file structure we've
    developed for things like passwords.

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Apply our secrets and put local.ini in the correct location
+python3 tmp/applysecrets.py
+
+# Launch our server, making it the process to ensure Docker behaves
+# http://www.projectatomic.io/docs/docker-image-author-guidance/
+# exec /usr/bin/couchdb
+exec couchdb
